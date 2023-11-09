5a28ccf03e1fa3cbcdd91457f3c8ba0ac43beaf7
yousef zamzamy <yousefzamzamy1191@gmail.com>
2023-11-09 15:55:00 +0200

    ننن

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {
   createBrowserRouter,
-  RouterProvider,
+  RouterProvider, 
 } from "react-router-dom";
 
 import Home from './pages/home';
